c208282d914c6c9569632bc77da0911ee0de7541
Ziyuan Liu <lutepluto@gmail.com>
2015-08-19 00:55:14 +0800

    add functional spies solution

---
+++ functional-javascript/12FunctionSpies.js
@@ -0,0 +1,18 @@
+'use strict'
+
+function Spy(target, method) {
+  var originalFunction = target[method]
+
+  var result = {
+    count: 0
+  }
+
+  target[method] = function() {
+    result.count++
+    return originalFunction.apply(this, arguments)
+  }
+
+  return result
+}
+
+module.exports = Spy
